a8d3f1328825e0841b1d56d106a0225da6a7cc53
Jesse Vugteveen <jesse.vugteveen@student.nhlstenden.com>
2023-03-17 12:12:41 +0100

    autoformatted code

--- src/main/java/com/api/parser/controller/JSONController.java
+++ src/main/java/com/api/parser/controller/JSONController.java
@@ -1,7 +1,7 @@
 package com.api.parser.controller;
 
 import com.api.parser.parser.JSONParser;
-import com.api.parser.parser.XMLParser;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,23 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-public class JsonController {
-
-    private final XMLParser xmlParser;
+public class JSONController {
     private final JSONParser jsonParser;
+
     @Autowired
-    public JsonController(){
-        this.xmlParser = new XMLParser();
+    public JSONController() {
         this.jsonParser = new JSONParser();
     }
 
     @PostMapping("/MT940toJSON")
-    public String MT940toJSON(@RequestParam("file") MultipartFile file){
-        return jsonParser.parseMT940(file);
-    }
-
-    @PostMapping("/MT940toXML")
-    public String MT940toXML(@RequestParam("file") MultipartFile file){
-        return xmlParser.parseMT940(file);
+    public JSONObject MT940toJSON(@RequestParam("file") MultipartFile file) {
+        return jsonParser.parseToFormat(file);
     }
 }
--- src/main/java/com/api/parser/controller/XMLController.java
+++ src/main/java/com/api/parser/controller/XMLController.java
@@ -1,2 +1,25 @@
-package com.api.parser.controller;public class XMLController {
+package com.api.parser.controller;
+
+import com.api.parser.parser.XMLParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.w3c.dom.Document;
+
+@RestController
+public class XMLController {
+    private final XMLParser xmlParser;
+
+    @Autowired
+    public XMLController() {
+        this.xmlParser = new XMLParser();
+    }
+
+    @PostMapping("/MT940toXML")
+    public String MT940toXML(@RequestParam("file") MultipartFile file) {
+        return xmlParser.parseToFormat(file);
+    }
 }
