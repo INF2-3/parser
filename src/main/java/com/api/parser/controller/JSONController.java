4665bca8a1d868c9101ead2ad65d3010f254eb3a
Noizeisdead <noizee829@gmail.com>
2023-03-29 10:11:00 +0200

    Changed MultipartFile to File

--- src/main/java/com/api/parser/controller/JSONController.java
+++ src/main/java/com/api/parser/controller/JSONController.java
@@ -3,10 +3,17 @@ package com.api.parser.controller;
 import com.api.parser.parser.JSONParser;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 public class JSONController {
@@ -18,7 +25,17 @@ public class JSONController {
     }
 
     @PostMapping("/MT940toJSON")
-    public String MT940toJSON(@RequestParam("file") MultipartFile file) {
-        return jsonParser.parseToFormat(file).toString(1);
+    public String MT940toJSON(@RequestParam("file") File file) throws IOException {
+        System.out.println(jsonParser.parseToFormat(convertFileToMultiPartFile(file)).toString(1));
+        return jsonParser.parseToFormat(convertFileToMultiPartFile(file)).toString(1);
     }
+
+    private MultipartFile convertFileToMultiPartFile(File file) throws IOException {
+        if (file == null || !file.exists() || !file.isFile()) {
+            return null;
+        }
+        InputStream stream = new FileInputStream(file);
+        return new MockMultipartFile("file", file.getName(), MediaType.TEXT_HTML_VALUE, stream);
+    }
+
 }
